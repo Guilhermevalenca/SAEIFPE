00e342291e5f61937042c6917578fe678579555e
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-09-14 21:11:58 -0300

    padronizando axios

--- front/src/main.js
+++ front/src/main.js
@@ -6,6 +6,7 @@
 
 // Components
 import App from './App.vue'
+import '@/plugins/axios';
 
 // Composables
 import { createApp } from 'vue'
---
+++ front/src/plugins/axios/index.js
@@ -0,0 +1,25 @@
+//arquivo de configuração do axios
+import axios from "axios";
+import Swal from "sweetalert2";
+
+const env = import.meta.env;
+
+const baseURL = (env.VITE_API_PREFIX + env.VITE_API_BASE_URL + ':' + env.VITE_API_PORT + '/' + env.VITE_API_SUFFIX).toString();
+
+axios.defaults.baseURL = baseURL;
+axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+axios.interceptors.request.use((config) => {
+  // console.log(config);
+  return config;
+});
+axios.interceptors.response.use(function (response) {
+  return response;
+},function (error) {
+  if(error.response.status === 429) {
+    Swal.fire({
+      title: 'Error',
+      text: 'Você foi proibido temporariamente de realizar algumas ações no site, por motivos de segurança'
+    });
+  }
+  return Promise.reject(error);
+});

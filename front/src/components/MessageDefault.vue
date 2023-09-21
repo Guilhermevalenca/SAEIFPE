5aabf28244c32fe7ee07a48ce3f3fc93f0fee783
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-09-21 19:03:05 -0300

    adicionando card default para mensagens

---
+++ front/src/components/MessageDefault.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card>
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-text>{{ text }}</v-card-text>
+    <v-icon v-if="success" icon="mdi-check-circle" />
+    <v-icon v-else icon="mdi-alert-circle-check-outline" />
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "MessageDefault",
+  props: {
+    title: String,
+    text: String,
+    success: Boolean
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>

44f3d9af814adc52f2d4df276e3cdcd50150a902
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-09-22 21:40:50 -0300

    ajustando formulario pre requisitos e envio de dados só com rules accept

--- front/src/components/user/RegistrationForm.vue
+++ front/src/components/user/RegistrationForm.vue
@@ -1,39 +1,48 @@
 <template>
   <v-card>
     <v-card-title>Formulário de cadastro</v-card-title>
-    <v-form @submit.prevent="registerRegistration()">
-      <v-card-text>
-        <v-text-field v-model="name" prepend-inner-icon="mdi-account-outline" label="Nome" placeholder="Digite seu nome" :rules="rules.name" required />
+    <v-card-text class="d-flex justify-center">
+      <v-form @submit.prevent="registerRegistration()" class="w-50">
+        <v-row>
+          <v-text-field v-model="name" prepend-inner-icon="mdi-account-outline" label="Nome completo" placeholder="Digite seu nome completo" :rules="rules.name" required />
+        </v-row>
 
-        <v-text-field v-model="email" prepend-inner-icon="mdi-email-outline" label="Email" placeholder="Digite seu email"  hint="exemplo@gmail.com" :rules="rules.email" required />
+        <v-row>
+          <v-text-field v-model="email" prepend-inner-icon="mdi-email-outline" label="Email pessoal" placeholder="Digite seu email"  hint="exemplo@gmail.com" :rules="rules.email" required />
+        </v-row>
 
-        <v-text-field v-model="cpf" label="cpf" placeholder="Digite seu cpf" hint="000.000.000-00"   :rules="rules.cpf" required />
+        <v-row>
+          <v-text-field v-model="cpf" label="CPF" placeholder="Digite seu cpf" hint="000.000.000-00"   :rules.="rules.cpf" required />
+        </v-row>
 
-        <v-text-field v-model="registration" label="Matricula" hint="Opcional" persistent-hint placeholder="digite o numero da matrícula" />
+        <v-row>
+          <v-text-field prepend-inner-icon="mdi-phone-outline" v-model.number="phone" label="Telefone(Opcional)" hint="(00) 00000-0000" persistent-hint placeholder="digite o numero do telefone" :counter="11" />
+        </v-row>
 
         <v-row>
           <v-col>
-            <v-select v-model="genre.selected" prepend-inner-icon="mdi-gender-male-female-variant" :items="genre.options" :rules="rules.genre.selected" required />
+            <v-select label="Gênero" v-model="genre.selected" prepend-inner-icon="mdi-gender-male-female-variant" :items="genre.options" :rules="rules.genre.selected" required />
           </v-col>
 
           <v-col v-if="genre.selected === 'Outro'">
             <v-text-field label="Gênero" placeholder="Digite seu gênero" v-model="genre.other" :rules="rules.genre.other" required />
           </v-col>
         </v-row>
+
         <v-row>
           <v-col>
             <v-text-field label="password" placeholder="Digite sua senha" v-model="password" prepend-inner-icon="mdi-lock-outline" :append-inner-icon="showIcon.password ? 'mdi-eye' : 'mdi-eye-off'" :type="showIcon.password ? 'text' : 'password'" @click:append-inner="showIcon.password = !showIcon.password" :rules="rules.password" required />
           </v-col>
 
           <v-col>
-            <v-text-field label="Confirme" placeholder="Confirme sua senha" persistent-placeholder prepend-inner-icon="mdi-lock-outline" :append-inner-icon="showIcon.confirmPassword ? 'mdi-eye' : 'mdi-eye-off'" :type="showIcon.confirmPassword ? 'text' : 'password'" @click:append-inner="showIcon.confirmPassword = !showIcon.confirmPassword" :rules="rules.genre.confirmPassword" required />
+            <v-text-field v-model="confirmPassword" label="Confirme senha" placeholder="Confirme sua senha" persistent-placeholder prepend-inner-icon="mdi-lock-outline" :append-inner-icon="showIcon.confirmPassword ? 'mdi-eye' : 'mdi-eye-off'" :type="showIcon.confirmPassword ? 'text' : 'password'" @click:append-inner="showIcon.confirmPassword = !showIcon.confirmPassword" :rules="rules.confirmPassword" required />
           </v-col>
         </v-row>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn :loading="loading" type="submit" color="secondary">Registrar</v-btn>
-      </v-card-actions>
-    </v-form>
+        <v-card-actions>
+          <v-btn :loading="loading" :disabled="!formInvalid" type="submit" color="secondary">Registrar</v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card-text>
   </v-card>
 </template>
 
@@ -48,7 +57,7 @@ export default {
       name: '',
       email: '',
       cpf: '',
-      registration: '',
+      phone: '',
       genre: {
         selected: '',
         options: [
@@ -60,6 +69,7 @@ export default {
         other: ''
       },
       password: '',
+      confirmPassword: '',
       showIcon: {
         password: false,
         confirmPassword: false
@@ -75,33 +85,33 @@ export default {
         ],
         email: [
           value => {
-            if(value) {
-              return true
-            }
-            return 'O email é obrigatório';
-          },
-          value => {
             if(/.+@.+\..+/.test(value)) {
               return true
             }
             return 'Email inválido';
+          },
+          value => {
+            if(value) {
+              if(!value.endsWith('@discente.ifpe.edu.br')) {
+                return true;
+              }
+              return 'Email não pode ser o email institucional';
+            }
+            return 'O email é obrigatório';
           }
         ],
         cpf: [
           value => {
             if(value) {
-              return true
+              const caseOne = value.length === 11 && /^\d+$/.test(value);
+              const caseTwo = value.length === 14 && ( /^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(value) );
+              if(caseOne || caseTwo) {
+                return true;
+              }
+              return 'cpf inválido';
             }
             return 'cpf obrigatório'
-          },
-          value => {
-            const caseOne = value.length === 11 && /^\d+$/.test(value);
-            const caseTwo = value.length === 14 && ( /^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(value) );
-            if(caseOne || caseTwo) {
-              return true;
-            }
-            return 'cpf inválido';
-          },
+          }
         ],
         genre: {
           selected: [
@@ -124,36 +134,33 @@ export default {
         password: [
           value => {
             if(value) {
-              return true;
+              if(/\d/.test(value) && /[a-zA-Z]/.test(value)) {
+                return true;
+              }
+              return 'Senha precisa ter caracteres alfanuméricos (letras e números)';
             }
-            return 'Senha obrigatória';
+            return 'Senha obrigatório';
           },
           value => {
-            if(/\d/.test(value) && /[a-zA-Z]/.test(value)) {
-              return true;
+            if(value) {
+              if(value.length >= 6) {
+                return true;
+              }
+              return 'Senha precisa ter no mínimo 6 caracteres';
             }
-            return 'Senha precisa ter caracteres alfanuméricos (letras e números)';
-          },
-          value => {
-            if(value.length >= 6) {
-              return true;
-            }
-            return 'Senha precisa ter no mínimo 6 caracteres';
+            return 'Senha obrigatório';
           }
         ],
         confirmPassword: [
           value => {
             if(value) {
-              return true;
+              if(value === this.password) {
+                return true;
+              }
+              return 'Senhas diferentes';
             }
             return 'É necessário confirmar sua senha';
-          },
-          value => {
-            if(value === this.password) {
-              return true;
-            }
-            return 'Senhas diferentes';
-          },
+          }
         ]
       }
     }
@@ -161,7 +168,7 @@ export default {
   methods: {
     registerRegistration() {
       this.loading = true;
-      const verifyCPF = (value) => {
+      const modifyCPF = (value) => {
         if(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(value)) {
           return value.replace(/[^0-9]/g, "");
         }
@@ -169,9 +176,9 @@ export default {
       }
       const data = {
         name: this.name,
-        cpf: verifyCPF(this.cpf),
+        cpf: modifyCPF(this.cpf),
         email: this.email,
-        registration: this.registration,
+        phone: this.phone,
         genre: this.genre.selected === 'Outro' ? this.genre.other : this.genre.selected,
         password: this.password
       }
@@ -184,8 +191,36 @@ export default {
         })
         .catch(error => console.log(error))
         .finally(() => this.loading = false);
-    }
+    },
   },
+  computed: {
+    formInvalid() {
+      const rulesName = this.rules.name.some(rule => rule(this.name) === true);
+      const rulesEmail = this.rules.email.some(rule => rule(this.email) === true);
+      const rulesCPF = this.rules.cpf.some(rule => rule(this.cpf) === true);
+      const rulesPassword = this.rules.password.some(rule => rule(this.password) === true);
+      const rulesConfirm = this.rules.confirmPassword.some(rule => rule(this.confirmPassword) ===true);
+      const rulesGenreSelected = this.rules.genre.selected.some(rule => {
+        if(this.genre.selected !== 'Outro') {
+          return rule(this.genre.selected) === true;
+        }
+        return false;
+      });
+      const rulesGenreOther = this.genre.other !== '' ? this.rules.genre.other.some(rule => rule(this.genre.other) === true) : false;
+      console.log(rulesName);
+      const rules = {
+        name: rulesName,
+        email: rulesEmail,
+        cpf: rulesCPF,
+        password: rulesPassword,
+        confirmPassword: rulesConfirm,
+        genreSelected: rulesGenreSelected,
+        genreOther: rulesGenreOther
+      };
+      console.table(rules);
+      return rulesName && rulesEmail && rulesCPF && (rulesPassword && rulesConfirm) && (rulesGenreSelected || rulesGenreOther);
+    }
+  }
 }
 </script>
 

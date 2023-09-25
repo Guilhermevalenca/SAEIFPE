2a4ad5dd3d0d11a90381bdb9b92df2fd4f005397
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-09-25 16:22:06 -0300

    Refatorando mensagem de logged com sucesso

---
+++ front/src/components/user/UserIsLoggedSuccess.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card width="500px">
+    <v-card-title>Cadastro realizado com sucesso</v-card-title>
+    <v-card-text>Obrigado por participar!</v-card-text>
+    <v-icon color="success" icon="mdi-check-circle" />
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "UserIsLoggedSuccess"
+}
+</script>
+
+<style scoped>
+
+</style>

4b8517398f44ba9acd57c2d9df63aabe212ef841
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-09-25 16:21:32 -0300

    Criando sobre da pagina

---
+++ front/src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <div>Sobre</div>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "AboutView"
+}
+</script>
+
+<style scoped>
+
+</style>

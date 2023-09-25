5cd260c4697837cfe1c393d48211f9678276358a
RodriguesClaudiane <113156051+RodriguesClaudiane@users.noreply.github.com>
2023-09-25 10:26:09 -0300

    adicionando alerta de aviso, fase: inicial

--- front/src/components/user/RegistrationForm.vue
+++ front/src/components/user/RegistrationForm.vue
@@ -1,4 +1,22 @@
 <template>
+  <v-dialog persistent width="900px" v-model="messageAlert">
+    <v-card>
+      <v-card-title  :class="['text-center', phoneDisplay ? 'ma-2' : 'ma-3', phoneDisplay ? '' : 'justify-md-center']">
+          Saudações, comunidade IFPE Campus Igarassu
+      </v-card-title>
+      <v-card-text class="ma-3">
+        <p>
+          Somos do terceiro período do curso Técnico em Informática para Internet, e através da disciplina de projeto e prática II estamos envolvidos em um projeto institucional, junto com a CREE (Coordenação de Relações Empresariais, Estágios e Egressos).
+        </p>
+        <p>
+          Nós e a instituição precisamos da ajuda de vocês para levantar dados sobre discentes e egressos, com o objetivo de trazer uma plataforma que una estes estudantes e possa fazer um acompanhamento de suas carreiras. Estamos aceitando todos(a) os(as) estudantes, mas se você está próximo da finalização do curso, sua adesão é imprescindível. Contamos com vocês!
+        </p>
+      </v-card-text>
+      <v-card-actions class="d-flex justify-center">
+        <v-btn class="mb-6" color="secondary" variant="outlined" @click="messageAlert = false">vamos lá!</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
   <v-card >
     <v-card-title class="text-center">Formulário de cadastro</v-card-title>
     <v-spacer class="pa-6" />
@@ -18,13 +36,13 @@
 
         <v-row>
           <v-col>
-            <v-text-field v-model="cpf" persistent-hint label="CPF" placeholder="Digite seu cpf" hint="000.000.000-00"   :rules.="rules.cpf" required />
+            <v-text-field v-model="cpf" persistent-hint prepend-inner-icon="mdi-card-account-details-outline" label="CPF" placeholder="Digite seu cpf" hint="000.000.000-00"   :rules.="rules.cpf" required />
           </v-col>
         </v-row>
 
         <v-row>
           <v-col :cols="phoneDisplayForCols" md="7">
-            <v-text-field prepend-inner-icon="mdi-phone-outline" persistent-hint v-model.number="phone.number" label="Telefone(Opcional)" hint="(00) 00000-0000" placeholder="digite o numero do telefone" :counter="11" />
+            <v-text-field prepend-inner-icon="mdi-phone-outline" persistent-hint v-model.number="phone.number" label="Telefone(Opcional)" hint="apenas números" placeholder="digite o número do telefone" :counter="11" />
           </v-col>
           <v-col md="4">
             <v-checkbox v-model="phone.wpp" label="Este número é whatsapp" />
@@ -43,7 +61,7 @@
 
         <v-row>
           <v-col :cols="phoneDisplayForCols">
-            <v-text-field label="password" placeholder="Digite sua senha" v-model="password" prepend-inner-icon="mdi-lock-outline" :append-inner-icon="showIcon.password ? 'mdi-eye' : 'mdi-eye-off'" :type="showIcon.password ? 'text' : 'password'" @click:append-inner="showIcon.password = !showIcon.password" :rules="rules.password" required />
+            <v-text-field label="Senha" placeholder="Digite sua senha" v-model="password" prepend-inner-icon="mdi-lock-outline" :append-inner-icon="showIcon.password ? 'mdi-eye' : 'mdi-eye-off'" :type="showIcon.password ? 'text' : 'password'" @click:append-inner="showIcon.password = !showIcon.password" :rules="rules.password" required />
           </v-col>
 
           <v-col>
@@ -74,7 +92,7 @@ export default {
         wpp: false
       },
       genre: {
-        selected: '',
+        selected: null,
         options: [
           'Masculino',
           'Feminino',
@@ -179,7 +197,9 @@ export default {
         ]
       },
       phoneDisplayForCols: window.innerWidth <= 800 ? '12' : '6',
-      phoneDisplayForForms: window.innerWidth <= 800 ? 'w-75' : 'w-50'
+      phoneDisplayForForms: window.innerWidth <= 800 ? 'w-75' : 'w-50',
+      messageAlert: true,
+      phoneDisplay: window.innerWidth <= 800
     }
   },
   methods: {

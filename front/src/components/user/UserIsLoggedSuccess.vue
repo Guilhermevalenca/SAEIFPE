6bf4ea6970e1f1a87b637999c4a8097987d44f56
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-09-26 21:07:58 -0300

    ajustando tela mobile

--- front/src/components/user/UserIsLoggedSuccess.vue
+++ front/src/components/user/UserIsLoggedSuccess.vue
@@ -1,14 +1,32 @@
 <template>
-  <v-card width="500px">
-    <v-card-title>Cadastro realizado com sucesso</v-card-title>
-    <v-card-text>Obrigado por participar!</v-card-text>
-    <v-icon color="success" icon="mdi-check-circle" />
+  <v-card width="500px" :class="[phoneDisplay ? 'w-75' : '']">
+    <v-card-title :class="['d-flex', phoneDisplay ? 'justify-space-between' : '']">
+      <div>
+        Cadastro realizado com sucesso
+      </div>
+      <v-icon color="success" icon="mdi-check-circle" />
+    </v-card-title>
+    <v-card-subtitle>
+      <div>
+        Obrigado por participar!
+      </div>
+    </v-card-subtitle>
+    <v-card-text class="d-flex text-body-1">
+      <div>
+        Muito obrigado por aderirem, pessoal! Em breve traremos novidades e funcionalidades para a aplicação. Esperaremos vocês nessas fases de testes para termos uma aplicação limpa e com boa usabilidade.
+      </div>
+    </v-card-text>
   </v-card>
 </template>
 
 <script>
 export default {
-  name: "UserIsLoggedSuccess"
+  name: "UserIsLoggedSuccess",
+  data() {
+    return {
+      phoneDisplay: window.innerWidth <= 800
+    }
+  }
 }
 </script>
 

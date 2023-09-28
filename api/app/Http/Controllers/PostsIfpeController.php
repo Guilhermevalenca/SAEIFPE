54f5ebedae4acf5f8b9921f3775a004d1bffcfff
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-09-28 17:04:52 -0300

    preparando posts na api

---
+++ api/app/Http/Controllers/PostsIfpeController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\postsIfpe;
+use Illuminate\Http\Request;
+
+class PostsIfpeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(postsIfpe $postsIfpe)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, postsIfpe $postsIfpe)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(postsIfpe $postsIfpe)
+    {
+        //
+    }
+}
--- api/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('genre')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('user_category')->default('user');
             $table->rememberToken();
             $table->timestamps();
         });

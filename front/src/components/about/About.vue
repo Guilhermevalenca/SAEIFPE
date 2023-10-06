e14f5484f9946235340330b8627114e2e91be667
AliceManguinho <118902832+AliceManguinho@users.noreply.github.com>
2023-10-06 11:40:42 -0300

    alteração do sobre

--- front/src/components/about/About.vue
+++ front/src/components/about/About.vue
@@ -1,8 +1,6 @@
 <template>
-
-        <p class="odio"> SAEIFPE é uma aplicação em desenvolvimento pelos alunos do IFPE Campus-Igarassu na matéria de Projeto e Prática. Seu principal objetivo é manter o contato com seus alunos egressos por meio de um ambiente interativo onde todos os concluintes poderão trocar experiências, receber novidades sobre cursos e empregos enviados pelo próprio instituto e entre outras coisas que estão em processo de criação. O instituto Federal deseja continuar mantendo a comunicação com seus estudantes para que a parceria entre aluno e instuição possa ser prolongada e ambos se ajudem, egressos que estão no mercado de trabalho podem contribuir com sua experiência para com os recem formandos, enquanto o instituto poderá fazer um acompanhamento de carreira. </p>
-
- 
+        <p class="odio"> SAEIFPE é uma aplicação em desenvolvimento pelos alunos do IFPE Campus-Igarassu na matéria de Projeto e Prática. Seu principal objetivo é manter o contato com seus alunos egressos por meio de um ambiente interativo onde todos os concluintes poderão trocar experiências, receber novidades sobre cursos e empregos enviados pelo próprio instituto e entre outras coisas que estão em processo de criação. O instituto Federal deseja continuar mantendo a comunicação com seus estudantes para que a parceria entre aluno e instuição possa ser prolongada e ambos se ajudem, egressos que estão no mercado de trabalho podem contribuir com sua experiência para com os recem formandos, enquanto o instituto poderá fazer um acompanhamento de carreira. Estamos aceitando todos(a) os(as) estudantes, mas se você está próximo da finalização do curso, sua adesão é imprescindível.</p>
+        <p class="tristeza"></p>
 </template>
 
 <style>
@@ -21,7 +19,14 @@
     padding: 50px;
     margin: 0 auto;
     text-align: justify;
-
+    
 }
+        @media screen and (max-width: 600px) {
+            .odio {
+                width: 90%; 
+        font-size: medium; 
+        padding: 20px; 
+            }
+        }
  
 </style>

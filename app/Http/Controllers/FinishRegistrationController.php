2ea8c135a978c104acedb45017b4383d97ec5278
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-11-06 15:32:17 -0300

    adicionando cadastro para usuários estudantes

--- app/Http/Controllers/FinishRegistrationController.php
+++ app/Http/Controllers/FinishRegistrationController.php
@@ -2,12 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\UsersGraduates;
+use App\Models\UsersStudying;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class FinishRegistrationController extends Controller
 {
     public function index() {
         return Inertia::render('FinishRegistration');
+    }
+    public function storeStudent(Request $request)
+    {
+        $validation = $request->validate([
+            'email_institution' => ['required', 'email'],
+            'enrollment' => ['required','string'],
+            'course' => ['required','string']
+        ]);
+        $validation['users_id'] = Auth::id();
+
+        DB::beginTransaction();
+
+        try {
+
+            UsersStudying::create($validation);
+
+            $user = User::find(Auth::id());
+            $user->update([
+                'role' => 'student'
+            ]);
+
+        } catch (\Error $e) {
+            DB::rollBack();
+        }
+
+        DB::commit();
+
+        return redirect()->route('home');
     }
 }
--- routes/web-routes/finishRegistrationRoute.php
+++ routes/web-routes/finishRegistrationRoute.php
@@ -3,9 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::controller(\App\Http\Controllers\FinishRegistrationController::class)
+    ->middleware('auth')
     ->group(function () {
 
         Route::get('finishRegistration','index')->name('finishRegistration_index');
+
+        Route::post('finishRegistrationStudent','storeStudent')->name('finishRegistration_storeStudent');
 
     });
 

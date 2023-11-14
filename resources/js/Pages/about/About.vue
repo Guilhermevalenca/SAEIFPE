2a326bd286c1d2dc50ef09a91c0e229358bf9e0f
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-11-14 11:10:57 -0300

    up

--- resources/js/Pages/about/About.vue
+++ resources/js/Pages/about/About.vue
@@ -7,7 +7,7 @@
                     <div class="odio"> SAEIFPE é uma aplicação em desenvolvimento pelos alunos do IFPE Campus-Igarassu na matéria de Projeto e Prática. Seu principal objetivo é manter o contato com seus alunos egressos por meio de um ambiente interativo onde todos os concluintes poderão trocar experiências, receber novidades sobre cursos e empregos enviados pelo próprio instituto e entre outras coisas que estão em processo de criação. O instituto Federal deseja continuar mantendo a comunicação com seus estudantes para que a parceria entre aluno e instuição possa ser prolongada e ambos se ajudem, egressos que estão no mercado de trabalho podem contribuir com sua experiência para com os recem formandos, enquanto o instituto poderá fazer um acompanhamento de carreira. Estamos aceitando todos(a) os(as) estudantes, mas se você está próximo da finalização do curso, sua adesão é imprescindível.</div>
                 </v-card-text>
             </v-card>
-            <div class="d-flex justify-center ">
+            <div :class="[$phoneDisplay ? '' : 'd-flex justify-center']">
                 <v-list :width="$phoneDisplay ? '': '200'" class="perfil ma-5">
                     <v-list-item @click="showDescription('Alice Manguinho')" class="d-flex justify-center">
                         <v-avatar size="120">

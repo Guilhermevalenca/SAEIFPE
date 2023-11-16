2ee691dfcbe28a84d4b3213d9d27cc55d42e8fc4
Claudiane Rodrigues <cra@discente.ifpe.edu.br>
2023-11-16 00:13:44 -0300

    File input funcionando no front e preview da imagem inserida
    Só no front mesmo porque minha incompetência é do tamanho do universo :)

--- resources/js/Components/depositions/AddDeposition.vue
+++ resources/js/Components/depositions/AddDeposition.vue
@@ -2,11 +2,11 @@
 <v-card id="Add-card" class="d-flex flex-column justify-center pa-10 w-75 h-auto rounded-xl" variant="flat" :style="'border: solid 1px #2E8429;'">
     <v-form @submit.prevent="submitDeposition()">
         <v-card-text class="d-flex pa-0 flex-column">
-    <v-textarea v-model="deposition.content" persistent-placeholder placeholder="Digite aqui seu depoimento" variant="outlined" :style="'border:#2E8429;'" auto-grow class="mb-0 rounded-xl"></v-textarea>
+    <v-textarea v-model="deposition.content" persistent-placeholder placeholder="Digite aqui seu depoimento" variant="flat" auto-grow class="mb-0 rounded-xl"></v-textarea>
+            <img :src="imageUrl" v-if="imageUrl" alt="ovo"/>
             <v-container id="test" class="pa-0 justify-start">
                 <v-file-input v-model="deposition.picture" prepend-icon="mdi mdi-image-plus" accept="image/png, image/jpg, image/bmp"></v-file-input>
             </v-container>
-            <img :src="imageUrl" v-if="imageUrl" alt="ovo"/>
         </v-card-text>
     <div class="d-flex justify-end ma-0 pa-2">
         <v-btn @click="$emit('cancelAction')" color="tertiary" variant="outlined">Cancelar</v-btn>
@@ -26,7 +26,7 @@ export default {
             imageUrl: null,
             deposition:useForm({
                 content:"",
-                picture:"",
+                picture: null,
             })
         }
     },
@@ -36,16 +36,16 @@ export default {
             this.deposition.content= ""
             this.deposition.picture= ""
             this.$emit('send')
-        },
-        watch:{
-            "deposition.picture":{
-                handler($new){
-                    if($new){
-                        this.imageUrl = URL.createObjectURL($new[0]);
-                    }
-                },
-                deep: true
-            }
+        }
+            },
+    watch:{
+        "deposition.picture":{
+            handler($new){
+                if($new){
+                    this.imageUrl = URL.createObjectURL($new[0]);
+                }
+            },
+            deep: true
         }
     }
 }
@@ -59,9 +59,6 @@ export default {
     .v-input{
         font-size: 1.6em;
         color: #2E8429;
-    }
-    .v-input-field{
-        display-inside: ruby;
     }
 }
 </style>

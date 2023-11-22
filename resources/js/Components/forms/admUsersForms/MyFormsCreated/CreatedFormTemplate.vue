568eacb372a2189b3af1b46c10b6a6b7f7041f16
Guilhermevalenca <guilherm3vrp@gmail.com>
2023-11-21 21:36:35 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- resources/js/Components/forms/admUsersForms/MyFormsCreated/CreatedFormTemplate.vue
+++ resources/js/Components/forms/admUsersForms/MyFormsCreated/CreatedFormTemplate.vue
@@ -1,6 +1,6 @@
 <template>
     <v-row>
-        <v-col :cols="$sreenMediumDisplay ? 7 : 7">
+        <v-col cols="8">
             <v-card-title>{{ form.title }}</v-card-title>
         </v-col>
         <v-col>
